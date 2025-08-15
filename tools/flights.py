d5cdd7285085518d020866b430b18642be21c7df
Muhammad Haris Khan <160781149+developerweb987@users.noreply.github.com>
2025-08-15 21:31:54 +0500

    Delete tools/flights.py

--- tools/flights.py
+++
@@ -1,7 +0,0 @@
-def get_flights(destination):
-    flights = [
-        f"Flight to {destination} at 10:00 AM",
-        f"Flight to {destination} at 3:00 PM",
-        f"Flight to {destination} at 8:00 PM"
-    ]
-    return flights
